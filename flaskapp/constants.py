bad5e1f0aad1271c7c38d5d5dd7f09aaf4395c6a
Sambhav Kothari <sambhavs.email@gmail.com>
2019-01-27 20:37:10 +0000

    Fix bug with Solr URI in flask app

--- flaskapp/constants.py
+++ flaskapp/constants.py
@@ -4,5 +4,5 @@ DIR_PATH = Path(__file__).parent
 DATA_DIR_PATH = DIR_PATH / "data"
 RANKERS_PATH = DATA_DIR_PATH / "rankers.json"
 ANNOTATIONS_PATH = DATA_DIR_PATH / "annotations.json"
-SOLR_URI = "http://localhost:8983/solr/wikipedia"
+SOLR_URI = "http://solr:8983/solr/wikipedia"
 DEFAULT_RANKER = "originalScoreModel"
