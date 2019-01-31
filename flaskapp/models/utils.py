c7d39f8e2a5a0318b105900acaf133aac9fc07da
Sambhav Kothari <sambhavs.email@gmail.com>
2019-01-31 21:42:45 +0000

    Add a CLI to manage the Web App and train models

--- flaskapp/models/utils.py
+++ flaskapp/models/utils.py
@@ -1,5 +1,7 @@
+from urllib.parse import urljoin
 import requests
-from flaskapp.constants import SOLR_MODEL_STORE_URL
+from flaskapp.constants import SOLR_MODEL_STORE_URL, DEFAULT_RANKER
+from flaskapp.query import get_rankers
 
 
 def upload_model(name, model_json):
@@ -12,3 +14,18 @@ def upload_model(name, model_json):
         raise Exception(
             f"Unable to upload model to Solr. Status code: {response.status_code}"
         )
+
+
+def delete_model(name):
+    if name == DEFAULT_RANKER:
+        raise Exception("Unable to delete default model")
+    response = requests.delete(urljoin(SOLR_MODEL_STORE_URL, name))
+    if response.status_code != 200:
+        raise Exception(f"Unable to delete model. Status code: {response.status_code}")
+
+
+def delete_all_models():
+    for model in get_rankers():
+        if model == DEFAULT_RANKER:
+            continue
+        delete_model(model)
